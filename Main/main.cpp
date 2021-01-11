b65407ab0dc545a464a8a649c99fbc105509346f
Pat <63267576+PatLP@users.noreply.github.com>
2021-01-11 18:30:26 -0300

    ASCII

---
+++ Main/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdlib.h>
+
+
+using namespace std;
+
+
+int main(){
+
+system("COLOR 0A");
+cout<<"  ___        ___________"<<endl;
+cout<<" / _ \\      |  ___|  _  \\"<<endl;
+cout<<"/ /_\\ \\_   _| |__ | | | |"<<endl;
+cout<<"|  _  | | | |  __|| | | |"<<endl;
+cout<<"| | | | |_| | |___| |/ /"<<endl;
+cout<<"\\_| |_/\\__, \\____/|___/"<<endl;
+cout<<"        __/ |"<<endl;
+cout<<"       |___/"<<endl;
+
+return 0;
+}
+
